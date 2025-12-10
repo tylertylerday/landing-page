224c482dde1862efdfe4f9477ac63d70f416c6ae
Tyler <tylerday89@gmail.com>
2025-12-10 14:29:27 -0800

    Fix mobile font size schedule

--- components/ScheduleCallSection.tsx
+++ components/ScheduleCallSection.tsx
@@ -50,8 +50,8 @@ const ScheduleCallSection: React.FC<ScheduleCallSectionProps> = ({
                     {/* Content */}
                     <div className="relative z-10 flex flex-col items-center text-center max-w-4xl mx-auto gap-6 md:gap-8 px-6 md:px-12 py-12 md:py-20">
                         {/* Main Headline */}
-                        <h3 className="text-4xl md:text-4xl font-bold text-white font-heading">
-                            Being a creator is hard, we can help.
+                        <h3 className="text-2xl md:text-4xl font-bold text-white font-heading">
+                            Being a creator is hard, <br /> we can help.
                         </h3>
 
                         {/* Body Text */}
